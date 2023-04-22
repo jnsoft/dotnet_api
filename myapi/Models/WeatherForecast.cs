6f000015e4abf7e5fbc239f1f53792942e35b39e
Johan <jnsoft@users.noreply.github.com>
2023-04-22 12:42:18 +0000

    refactor

---
+++ myapi/Models/WeatherForecast.cs
@@ -0,0 +1,19 @@
+namespace Models;
+
+public record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    private static readonly string[] summaries = new[]{"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"};
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public static IEnumerable<WeatherForecast> GenerateWeatherReport()
+    {
+        return Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateTime.Now.AddDays(index),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            )
+        ).ToArray();
+    }
+}
--- myapi/Program.cs
+++ myapi/Program.cs
@@ -1,4 +1,5 @@
 using myapi.Authentication;
+using Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,30 +19,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 var wgroup = app.MapGroup("weather").AddEndpointFilter<ApiKeyEndpointFilter>();
 
 wgroup.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return WeatherForecast.GenerateWeatherReport();
 })//.add
 .WithName("GetWeatherForecast");
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
